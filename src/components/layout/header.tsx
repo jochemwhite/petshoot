1b62bacc654399f0d462dac9b168d9575da1fca9
jochemwhite <j.vanderwit@amrio.nl>
2024-06-22 16:46:20 +0200

    added a hamburger menu for mobile

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -3,14 +3,22 @@ import { cn } from "@/lib/utils";
 import { useScroll } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
-import { HamburgerMenu } from "./hamburger-menu";
+import { HamburgerMenu } from "../nav/hamburger-menu/hamburger-menu";
+
+export interface Link {
+  name: string;
+  href: string;
+}
+
 
 export default function Header() {
   const { scrollYProgress } = useScroll();
   const [scrollY, setScrollY] = useState<number>(0);
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const links = [
+ 
+
+  const links: Link[] = [
     {
       name: "Home",
       href: "/",
@@ -33,21 +41,16 @@ export default function Header() {
     setScrollY(e);
   });
 
-  const toggleHamMenu = () => setIsOpen(!isOpen);
-  const genericHamburgerLine = `h-1 w-6 my-1 rounded-full bg-black transition ease transform duration-300`;
   return (
     <header
-      className={cn("fixed z-50 h-full w-full text-secondary-text text-xl", {
+      className={cn("fixed z-50 h-20 w-full text-secondary-text text-xl", {
         "backdrop-blur-md": scrollY > 0.1,
       })}
-      >
+    >
       {/* hamburger menu */}
-      <HamburgerMenu />
+      <HamburgerMenu links={links} />
 
-
-
-      <nav className="h-full relative">
-        
+      <nav className="hidden lg:block h-full relative">
         {/* desktop menu */}
 
         <ul className="hidden lg:flex h-full justify-center items-center ">
